309a965c8cb57e9af9676386fda985e166fd9a99
Andrea Cadeddu <andrea.cadeddu@northwestern.edu>
2013-08-28 11:35:30 -0500

    adn one

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,10 @@
 'use strict';
 
 angular.module('showmethedomApp', [])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider,$httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    // Set up router
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,65 @@
 'use strict';
 
 angular.module('showmethedomApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.d3stuff = "<svg></svg>"
+  .controller('MainCtrl', function ($scope, $http) {
+  	$scope.d3 = function (data) {
+  		//JSON object with the data
+      var treeData = {"name" : "A", "info" : "tst", "children" : [
+			{"name" : "A1" },
+			{"name" : "A2" },
+			{"name" : "A3", "children": [
+			      {"name" : "A31", "children" :[
+			{"name" : "A311" },
+			{"name" : "A312" }
+	]}] }
+      ]};
+
+      // Create a svg canvas
+      var vis = d3.select("#result").append("svg:svg")
+	  .attr("width", 400)
+	  .attr("height", 300)
+	  .append("svg:g")
+	  .attr("transform", "translate(40, 0)"); // shift everything to the right
+
+      // Create a tree "canvas"
+      var tree = d3.layout.tree()
+	.size([300,150]);
+
+      var diagonal = d3.svg.diagonal()
+	  // change x and y (for the left to right tree)
+	  .projection(function(d) { return [d.y, d.x]; });
+
+      // Preparing the data for the tree layout, convert data into an array of nodes
+      var nodes = tree.nodes(treeData);
+      // Create an array with all the links
+      var links = tree.links(nodes);
+
+      var link = vis.selectAll("pathlink")
+	  .data(links)
+	  .enter().append("svg:path")
+	  .attr("class", "link")
+	  .attr("d", diagonal)
+
+      var node = vis.selectAll("g.node")
+	  .data(nodes)
+	  .enter().append("svg:g")
+	  .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+
+      // Add the dot at every node
+      node.append("svg:circle")
+	  .attr("r", 3.5);
+
+      // place the name atribute left or right depending if children
+      node.append("svg:text")
+	  .attr("dx", function(d) { return d.children ? -8 : 8; })
+	  .attr("dy", 3)
+	  .attr("text-anchor", function(d) { return d.children ? "end" : "start"; })
+	  .text(function(d) { return d.name; })
+  	};
+    $scope.submit = function () {
+		$http.get(this.mainURL).success(function (data) {
+			$scope.d3(data);
+		});
+    };
+    $scope.d3stuff = "<h1>HEEH</h1>"
   });
