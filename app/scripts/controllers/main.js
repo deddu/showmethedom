ba2ca6c67a1283e9943dd09ea09b346cdc3cd2de
Matthew Wampler-Doty <matt@w-d.org>
2013-08-29 14:41:55 -0500

    Moving json generation to server side

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,67 +1,76 @@
 'use strict';
 
 angular.module('showmethedomApp')
-.controller('MainCtrl', function ($scope, $http) {
-	$scope.d3 = function (xml) {
+	.controller('MainCtrl', function($scope, $http) {
+	$scope.d3 = function(treeData) {
+		// Create a svg canvas
+		var height = 1000,
+			width = 400;
+			
+		d3.select("#d3stuff")
+			.select('svg')
+			.remove();
+			
+		var vis = d3.select("#d3stuff")
+			.append('svg')
+			.attr("height", height)
+			.attr("width", width * 2)
+			.append("svg:g")
+			.attr("transform", "translate(200, 0)"); // shift everything to the right
 
-	  var jqDOMtoJSON = function( $dom ) {
-			return { name: $dom.nodeName ,
-					 children: jQuery.map($dom.children, jqDOMtoJSON) };
-	  };
+		// Create a tree "canvas"
+		var tree = d3.layout.tree()
+			.size([height, width]);
 
-	  var treeData = jqDOMtoJSON(jQuery(jQuery.parseHTML(xml)).get()[0].parentNode);
+		var diagonal = d3.svg.diagonal()
+		// change x and y (for the left to right tree)
+		.projection(function(d) {
+			return [d.y, d.x];
+		});
 
-      // Create a svg canvas
-      var height = 1000,
-          width = 400;
-      d3.select("#d3stuff").select('svg').remove()
-      var vis = d3.select("#d3stuff")
-                  .append('svg')
-                  .attr("height", height)
-                  .attr("width", width*2)
-                  .append("svg:g")
-	              .attr("transform", "translate(200, 0)"); // shift everything to the right
+		// Preparing the data for the tree layout, convert data into an array of nodes
+		var nodes = tree.nodes(treeData);
+		// Create an array with all the links
+		var links = tree.links(nodes);
 
-      // Create a tree "canvas"
-      var tree = d3.layout.tree()
-                   .size([height,width]);
+		var link = vis.selectAll("pathlink")
+			.data(links)
+			.enter()
+			.append("svg:path")
+			.attr("class", "link")
+			.attr("d", diagonal)
 
-      var diagonal = d3.svg.diagonal()
-	                   // change x and y (for the left to right tree)
-	                   .projection(function(d) { return [d.y, d.x]; });
+		var node = vis.selectAll("g.node")
+			.data(nodes)
+			.enter()
+			.append("svg:g")
+			.attr("transform", function(d) {
+			return "translate(" + d.y + "," + d.x + ")";
+		})
 
-      // Preparing the data for the tree layout, convert data into an array of nodes
-      var nodes = tree.nodes(treeData);
-      // Create an array with all the links
-      var links = tree.links(nodes);
+		// Add the dot at every node
+		node.append("svg:circle")
+			.attr("r", 3.5);
 
-      var link = vis.selectAll("pathlink")
-					  .data(links)
-					  .enter().append("svg:path")
-					  .attr("class", "link")
-					  .attr("d", diagonal)
+		// place the name atribute left or right depending if children
+		node.append("svg:text")
+			.attr("dx", function(d) {
+			return d.children ? -8 : 8;
+		})
+			.attr("dy", 3)
+			.attr("text-anchor", function(d) {
+			return d.children ? "end" : "start";
+		})
+			.text(function(d) {
+			return d.name;
+		})
+	};
 
-      var node = vis.selectAll("g.node")
-      .data(nodes)
-      .enter().append("svg:g")
-      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+	$scope.submit = function() {
+		$http.get("/gettree?url=" + this.mainURL)
+			.success(function(data) {
+			$scope.d3(data);
+		});
+	}
 
-      // Add the dot at every node
-      node.append("svg:circle")
-      .attr("r", 3.5);
-
-      // place the name atribute left or right depending if children
-      node.append("svg:text")
-      .attr("dx", function(d) { return d.children ? -8 : 8; })
-      .attr("dy", 3)
-      .attr("text-anchor", function(d) { return d.children ? "end" : "start"; })
-      .text(function(d) { return d.name; })
-  };
-
-  $scope.submit = function () {
-  	$http.get("/geturl?url="+this.mainURL).success(function (data) {
-  		$scope.d3(data);
-  	});
-  }
-  
 });
